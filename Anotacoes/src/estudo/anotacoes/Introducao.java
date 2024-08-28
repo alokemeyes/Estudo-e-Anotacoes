565771f6fc633042490f47ee8da4107a522e07b8
MarcosWPSilva <18124974+Reveroph@users.noreply.github.com>
2024-08-28 20:53:36 -0300

    Update Introducao.java

--- Anotacoes/src/estudo/anotacoes/Introducao.java
+++ Anotacoes/src/estudo/anotacoes/Introducao.java
@@ -4,7 +4,6 @@ public class Introducao {
 
 	public static void main(String[] args) {
 		System.out.println("Bons estudos, e anote sem medo!");
-		System.out.println("Que tal mais uma linha aparecendo aqui?");
 	}
 
 }
