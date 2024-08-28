2620ad922924a8fa01cfdbc522da0d9d8dc8aeb0
MarcosWPSilva <18124974+Reveroph@users.noreply.github.com>
2024-08-28 20:12:20 -0300

    Adicionando mais uma descrição

--- Anotacoes/src/estudo/anotacoes/Introducao.java
+++ Anotacoes/src/estudo/anotacoes/Introducao.java
@@ -4,6 +4,7 @@ public class Introducao {
 
 	public static void main(String[] args) {
 		System.out.println("Bons estudos, e anote sem medo!");
+		System.out.println("Que tal mais uma linha aparecendo aqui?");
 	}
 
 }
