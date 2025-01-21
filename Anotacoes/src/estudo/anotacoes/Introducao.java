e1ac504301e3df61d751cbdc44facef2bf3a11b2
Christiano <christiano.stsj@gmail.com>
2025-01-21 16:06:02 -0300

    Configurando o intellij

--- Anotacoes/src/estudo/anotacoes/Introducao.java
+++ Anotacoes/src/estudo/anotacoes/Introducao.java
@@ -5,6 +5,7 @@ public class Introducao {
 	public static void main(String[] args) {
 		System.out.println("Bons estudos, e anote sem medo!");
 		System.out.println("Que tal mais uma linha aparecendo por aqui?");
+		system.out.println("Agora utilizando o Intellij");
 	}
 
 }
